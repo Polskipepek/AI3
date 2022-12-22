0be0b82c8734fba07781540a9cbcc8c165717a00
Michał Stasinkiewicz <lonely.navel@gmail.com>
2022-12-23 00:13:28 +0100

    Smiga

--- AI3/ILAAlgorithm/InductiveLearningAlgorithm.cs
+++ AI3/ILAAlgorithm/InductiveLearningAlgorithm.cs
@@ -1,22 +1,21 @@
 ﻿using AI3.Entities;
-using System.Collections;
 
 namespace AI3.ILAAlgorithm {
     internal class InductiveLearningAlgorithm {
         public List<Rule> Learn(IEnumerable<Entity> data) {
             var ruleset = new List<Rule>();
             var subarrays = new Dictionary<string, List<Entity>>();
-            var pairs = new Hashtable();
+            Dictionary<IEnumerable<ILAAttribute>, int> pairs = new();
+            int prevNumOfAttributes = 0;
 
             DivideArrayIntoSubarrays(data, subarrays);
 
             foreach (var subarray in subarrays.Values) {
                 int maxCount = 0;
-                List<ILAAttribute> maxCombination = new();
+                IEnumerable<ILAAttribute> maxCombination = new List<ILAAttribute>();
                 int numAttributeCombinations = 1;
 
                 var otherEntities = subarrays.Where(x => !x.Value.Equals(subarray)).SelectMany(x => x.Value);
-                int prevNumOfAttributes = 0;
                 var currentCombinations = GetCombinations(subarray, numAttributeCombinations);
 
                 while (!subarray.All(x => x.IsClassified)) {
@@ -37,7 +36,13 @@ namespace AI3.ILAAlgorithm {
                                     a.Name.Equals(c)
                                     && a.Value.Equals(entity.Attributes.First(at => at.Name.Equals(c)).Value)))));
 
-                        GetMaxCombinationFromEntities(pairs, ref maxCount, ref maxCombination, combination, entities);
+                        if (entities.Any()) {
+                            var localMax = GetMaxCombinationFromEntities(pairs, combination, entities, out int counter);
+                            if (counter > maxCount) {
+                                maxCombination = localMax;
+                                maxCount = counter;
+                            }
+                        }
                     }
 
                     if (!maxCombination.Any()) {
@@ -45,7 +50,7 @@ namespace AI3.ILAAlgorithm {
                     } else {
                         MarkClassified(subarray, maxCombination);
                         CreateRule(ruleset, subarray, maxCombination);
-                        maxCombination = new();
+                        maxCombination = Enumerable.Empty<ILAAttribute>();
                         maxCount = 0;
                     }
                 }
@@ -57,49 +62,28 @@ namespace AI3.ILAAlgorithm {
             return CombinationGenerator.GetCombinations(subarray.First().Attributes.Select(x => x.Name)).Where(x => x.Count() == numAttributeCombinations);
         }
 
-        private static void CreateRule(List<Rule> ruleset, List<Entity> subarray, List<ILAAttribute> maxCombination) {
+        private static void CreateRule(List<Rule> ruleset, IEnumerable<Entity> subarray, IEnumerable<ILAAttribute> maxCombination) {
             ruleset.Add(new Rule {
-                Attributes = maxCombination,
+                Attributes = maxCombination.ToList(),
                 DecisionAttribute = subarray.First(e => e.Attributes.All(a => !maxCombination.Any(c => c.Name.Equals(a.Name) && c.Value.Equals(a.Name)))).DecisionAttribute
             });
         }
-        private static void GetMaxCombinationFromEntities(Hashtable pairs, ref int maxCount, ref List<ILAAttribute> maxCombination, IEnumerable<string> combination, IEnumerable<Entity> entities) {
-            foreach (var entity in entities) {
-                var key = combination.GetHashCode();
-                if (!pairs.ContainsKey(key)) {
-                    pairs[key] = (1, combination.Select(c => new ILAAttribute { Name = c, Value = entity.Attributes.First(a => a.Name.Equals(c)).Value }).ToList());
-                } else {
-                    var (tcount, tattributesCombination) = (ValueTuple<int, List<ILAAttribute>>)pairs[key];
-                    pairs[key] = (tcount + 1, tattributesCombination);
-                }
 
-                var (count, attributesCombination) = (ValueTuple<int, List<ILAAttribute>>)pairs[key];
-                if (count > maxCount && entities.Any()) {
-                    maxCount = count;
-                    maxCombination = attributesCombination;
+        private static IEnumerable<ILAAttribute> GetMaxCombinationFromEntities(Dictionary<IEnumerable<ILAAttribute>, int> pairs, IEnumerable<string> combination, IEnumerable<Entity> entities, out int counter) {
+            foreach (var entity in entities) {
+                foreach (var column in combination) {
+                    var pair = pairs.FirstOrDefault(a => a.Key.Select(x => x.Name).Contains(column) && a.Key.Select(x => x.Value).Contains(entity.Attributes.FirstOrDefault(a => a.Name.Equals(column))?.Value));
+                    if (pair.Key == null) {
+                        pairs.Add(combination.Select(c => new ILAAttribute() { Name = c, Value = entity.Attributes.First(a => a.Name.Equals(c)).Value }), 1);
+                    } else {
+                        pairs[pair.Key]++;
+                    }
                 }
             }
+            var maxPair = pairs.OrderByDescending(x => x.Value).FirstOrDefault();
+            counter = maxPair.Value;
+            return maxPair.Key;
         }
-
-        //private static void GetMaxCombinationFromEntities(Dictionary<IEnumerable<ILAAttribute>, int> pairs, ref int maxCount, ref List<ILAAttribute> maxCombination, IEnumerable<string> combination, IEnumerable<Entity> entities) {
-        //    foreach (var entity in entities) {
-        //        foreach (var column in combination) {
-        //            var pair = pairs.FirstOrDefault(a => a.Key.Select(x => x.Name).Contains(column) && a.Key.Select(x => x.Value).Contains(entity.Attributes.FirstOrDefault(a => a.Name.Equals(column))?.Value));
-        //            if (pair.Key == null) {
-        //                pairs.Add(combination.Select(c => new ILAAttribute() { Name = c, Value = entity.Attributes.First(a => a.Name.Equals(c)).Value }), 1);
-        //            } else {
-        //                pairs[pair.Key]++;
-        //            }
-        //        }
-
-        //        var bestLocalCombination = pairs.OrderByDescending(x => x.Value).First();
-
-        //        if (bestLocalCombination.Value > maxCount && entities.Any()) {
-        //            maxCount = bestLocalCombination.Value;
-        //            maxCombination = combination.Select(name => new ILAAttribute { Name = name, Value = bestLocalCombination.Key.First(a => a.Name.Equals(name)).Value }).ToList();
-        //        }
-        //    }
-        //}
 
         private static void DivideArrayIntoSubarrays(IEnumerable<Entity> data, Dictionary<string, List<Entity>> subarrays) {
             foreach (var row in data) {
@@ -110,7 +94,7 @@ namespace AI3.ILAAlgorithm {
             }
         }
 
-        private static void MarkClassified(List<Entity> subarray, List<ILAAttribute> combination) {
+        private static void MarkClassified(IEnumerable<Entity> subarray, IEnumerable<ILAAttribute> combination) {
             foreach (var entity in subarray) {
                 bool found = true;
                 foreach (var attribute in combination) {
