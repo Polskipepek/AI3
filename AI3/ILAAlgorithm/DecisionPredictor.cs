73983b76aca75d3635340a434cc1750504930801
Michał Stasinkiewicz <lonely.navel@gmail.com>
2022-12-23 04:46:12 +0100

    decistion Predictor

---
+++ AI3/ILAAlgorithm/DecisionPredictor.cs
@@ -0,0 +1,20 @@
+﻿using AI3.Entities;
+
+namespace AI3.ILAAlgorithm {
+    public class DecisionPredictor {
+        private List<Rule> rules;
+
+        public void SetRules(IEnumerable<Rule> rules) {
+            this.rules = rules.OrderByDescending(r => r.Attributes.Count).ToList();
+        }
+
+        public string PredictDecision(Entity entity) {
+            foreach (var rule in rules) {
+                if (entity.Attributes.Count(a => rule.Attributes.Any(r => r.Name.Equals(a.Name) && r.Value.Equals(a.Value))) == rule.Attributes.Count) {
+                    return rule.DecisionAttribute;
+                }
+            }
+            return default;
+        }
+    }
+}
--- AI3/Program.cs
+++ AI3/Program.cs
@@ -7,19 +7,18 @@ Console.WriteLine("Cwiczenie 3!");
 Console.WriteLine();
 while (true) {
     Console.WriteLine("1. Example Data from presentation.");
-    Console.WriteLine("2. Test Banking Data.");
-    Console.WriteLine("3. Training Banking Data.");
-    Console.WriteLine("4. Test Fruits Data.TODO");
-    Console.WriteLine("5. Training Fruits Data.TODO");
-    Console.WriteLine("6. Custom Data");
+    Console.WriteLine("2. Banking Data.");
+    Console.WriteLine("3. Fruits Data. //TODO FIND DATASET");
+    Console.WriteLine("0. Custom Data");
 
     var input = Console.ReadKey();
     var data = GetData(input.Key);
-
     Console.Clear();
+
     Console.WriteLine($"There are {data.Count()} entities. Do you want to limit data? (Y - for yes; Any other key to continue with {data.Count()} entities.)");
     var limitFlag = Console.ReadKey().Key;
     Console.Clear();
+
     if (limitFlag == ConsoleKey.Y) {
         Console.WriteLine($"Number of entities (1-{data.Count()}): ");
         var entitycount = Console.ReadLine();
@@ -27,6 +26,7 @@ while (true) {
             if (result < data.Count() && result > 0) data = data.Take(result);
         }
     }
+
     Console.Clear();
 
     var ila = new InductiveLearningAlgorithm();
@@ -48,21 +48,34 @@ while (true) {
         Console.WriteLine($"\tDecision = {rules[i].DecisionAttribute}");
     }
 
+    if (input.Key == ConsoleKey.D2) {
+        DecisionPredictor predictor = new();
+        predictor.SetRules(rules);
+
+        var dataToPredict = DataReader.ReadData($"{Environment.CurrentDirectory}/Data/test.csv", ";");
+        var entities = TableToEntitiesMapper.Map(dataToPredict);
+        Console.WriteLine();
+        Console.WriteLine("Predictions:");
+
+        foreach (var entity in entities) {
+            var prediction = predictor.PredictDecision(entity);
+            if (prediction != null) {
+                Console.WriteLine(prediction);
+            }
+        }
+        Console.WriteLine("End of successful predictions.");
+
+    }
+
     Console.ReadKey();
     Console.Clear();
 }
 IEnumerable<Entity> GetData(ConsoleKey key) {
     if (key == ConsoleKey.D2) {
-        var data = DataReader.ReadData($"{Environment.CurrentDirectory}/Data/test - short.csv", ";");
-        return TableToEntitiesMapper.Map(data);
-    } else if (key == ConsoleKey.D3) {
         var data = DataReader.ReadData($"{Environment.CurrentDirectory}/Data/train.csv", ";");
         return TableToEntitiesMapper.Map(data);
-        //} else if (key == ConsoleKey.D4) {
 
-        //} else if (key == ConsoleKey.D5) {
-
-    } else if (key == ConsoleKey.D6) {
+    } else if (key == ConsoleKey.D0) {
         Console.Clear();
         Console.WriteLine("Decision attribute column name needs to be named \'decision\'");
         Console.WriteLine("Enter full path to the file. c:/users/alfa/desktop/dummydata.csv");
@@ -72,6 +85,7 @@ IEnumerable<Entity> GetData(ConsoleKey key) {
         var data = DataReader.ReadData(path, delimeter.KeyChar.ToString());
         return TableToEntitiesMapper.Map(data);
 
+        //} else if (key == ConsoleKey.D3) {
     } else {
         var examples = new List<Entity> {
             new Entity { Attributes = new List<ILAAttribute>() { new ILAAttribute("Rozmiar", "sredni"), new ILAAttribute("Kolor", "niebieski"), new ILAAttribute("Ksztalt", "kostka") }, DecisionAttribute = "TAK" },
